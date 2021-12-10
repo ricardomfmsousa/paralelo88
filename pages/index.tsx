6a408b3b8daa56c09b5ddf2b46931c041e14d2b5
Ricardo Sousa <r.sousa@techfirm.pt>
2021-12-10 11:52:55 +0000

    Add favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,27 @@ export default function Home(): JSX.Element {
     <div className="container">
       <Head>
         <title>Paralelo 88</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />{' '}
       </Head>
 
       <main>
