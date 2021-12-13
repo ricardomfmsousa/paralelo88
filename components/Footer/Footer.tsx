8c11b4dee61dea1c29c9dd66e44795e48158bcb2
Ricardo Sousa <r.sousa@techfirm.pt>
2021-12-13 18:41:47 +0000

    Update footer info

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,26 +1,29 @@
 import classNames from "classnames";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import { SocialIcon } from "react-social-icons";
 
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <Container
-      className={classNames(styles.footer, 'text-center my-auto py-auto')}
-    >
-      <Row id="social-icons">
-        <Col>
+    <div className={classNames(styles.footer, 'container py-5')}>
+      <div id="social-icons" className="row">
+        <div className="col-md-4">
+          <h3>Localização</h3>
+          <div>Rua Arronches Junqueiro, Nº 88 2900-248 Setúbal</div>
+        </div>
+        <div className="col-md-4 text-center">
           <SocialIcon
             url="https://www.instagram.com/paralelo_88"
             target="_blank"
             rel="noopener noreferrer"
             bgColor="black"
           />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+        <div className="col-md-4">
+          <h3>Contactos</h3>
+          <div>paralelo.oitentaeoito@gmail.com 968 830 915 963 264 895</div>
+        </div>
+      </div>
+    </div>
   )
 }
